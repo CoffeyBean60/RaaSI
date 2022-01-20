bcec746d321ea96921ccf7a2431f20d2fe4ee5d8
Michael Coffey <38055125+CoffeyBean60@users.noreply.github.com>
2022-01-20 15:08:18 -0600

    Add files via upload
    Initial commit of localnodesetup2.sh

---
+++ localnodesetup2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Beginning to join node to the master cluster."
+
+echo "Please run nodeSetup2.sh on the master server."
+
+join=$(nc -w 10 -l 3333)
+
+echo $join
+
+#$join
+
+echo "Congragulations, you have finished setting up this node."
