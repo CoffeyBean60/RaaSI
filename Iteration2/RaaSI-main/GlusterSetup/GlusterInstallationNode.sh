dc9518692ba6865c27e98647bd350e63e2bef119
CoffeyBean60 <coffeymichael33@yahoo.com>
2022-04-07 18:57:24 -0500

    shellcheck fix added on GlusterInstallationNode.sh

--- Iteration2/RaaSI-main/GlusterSetup/GlusterInstallationNode.sh
+++ Iteration2/RaaSI-main/GlusterSetup/GlusterInstallationNode.sh
@@ -8,7 +8,7 @@ apt-get install -y wget
 
 wget -O -https://download.gluster.org/pub/gluster/glusterfs/7/rsa.pub | apt-key add -
 
-echo deb [arch=amd64] https://download.gluster.org/pub/gluster/glusterfs/7/LATEST/Debian/buster/amd64/apt buster main > /etc/apt/sources.list.d/gluster.list
+echo "deb [arch=amd64] https://download.gluster.org/pub/gluster/glusterfs/7/LATEST/Debian/buster/amd64/apt buster main" > /etc/apt/sources.list.d/gluster.list
 
 apt-get update
 apt-get install -y glusterfs-server
