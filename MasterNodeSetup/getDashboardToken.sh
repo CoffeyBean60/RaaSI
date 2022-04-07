18b54c089d352a45fa3ba9842abeec00834ba816
Michael Coffey <38055125+CoffeyBean60@users.noreply.github.com>
2022-04-07 16:31:27 -0500

    Delete MasterNodeSetup directory

--- MasterNodeSetup/getDashboardToken.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Getting the dashboard token..."
-
-kubectl get secret $(kubectl get serviceaccount dashboard -o jsonpath="{.secrets[0].name}") -o jsonpath="{.data.token}" | base64 --decode
-
