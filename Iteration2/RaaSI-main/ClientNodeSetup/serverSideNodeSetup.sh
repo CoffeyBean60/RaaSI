0ec11ca28e365219bdaf25cab485b41f4a6adc91
Michael Coffey <38055125+CoffeyBean60@users.noreply.github.com>
2022-04-07 16:04:00 -0500

    Added device detection

--- Iteration2/RaaSI-main/ClientNodeSetup/serverSideNodeSetup.sh
+++ Iteration2/RaaSI-main/ClientNodeSetup/serverSideNodeSetup.sh
@@ -20,26 +20,46 @@ sed -i "s/$master_hostname/$master_hostname\\n$client_ip\\t$client_hostname/g" /
 
 echo "Adding the master to the /etc/hosts file on the client server..."
 
+#client_host_command=$(sed -i "s/$client_hostname/$client_hostname\\n$master_ip\\t$master_hostname/g" /etc/hosts)
+
 client_host_command="s/$client_hostname/$client_hostname\\\n$master_ip\\\t$master_hostname/g"
+echo $client_host_command
 
 ssh -t $client_user@$client_ip "sudo sed -i $client_host_command /etc/hosts"
 
 echo "Creating RaaSI directory on client machine..."
 
-ssh $client_user@$client_ip "mkdir RaaSI"
+ssh $client_user@$client_ip "mkdir /home/$client_user/RaaSI"
 
 echo "Transfering clientSideNodeSetup.sh script to client machine..."
 
-scp clientSideNodeSetup.sh $client_user@$client_ip:RaaSI/clientSideNodeSetup.sh
+scp clientSideNodeSetup.sh $client_user@$client_ip:/home/$client_user/RaaSI/clientSideNodeSetup.sh
 
 echo "Making clientSideNodeSetup.sh executable..."
 
 ssh -t $client_user@$client_ip "sudo chmod +x /home/$client_user/RaaSI/clientSideNodeSetup.sh"
 
-echo "Executing clientSideNodeSetup.sh on $client_hostname..."
+echo "Executin clientSideNodeSetup.sh on $client_hostname..."
 
-ssh -t $client_user@$client_ip "sudo RaaSI/clientSideNodeSetup.sh"
+ssh -t $client_user@$client_ip "sudo /home/$client_user/RaaSI/clientSideNodeSetup.sh"
 
 echo "$client_hostname joining the RaaSI cluster..."
 
 ssh -t $client_user@$client_ip "sudo $join_command"
+
+echo "Would you like to search for devices on this node (y/n)?"
+read response
+
+if [[ "Yy" =~ $response ]]; then
+	echo "Enter the device that you would like to search for: "
+	read device
+	device_spaceless=$(echo "$device" | sed 's/ //g')
+	device_present=$(ssh -t $client_user@$client_ip "sudo lspci" | grep "$device")
+        if [ -z "$device_present" ]; then
+        	echo "$device is not present";
+        else
+        	echo "$device is present"
+        	echo "Adding $device as a label to the node..."
+        	kubectl label nodes "$client_hostname" device="$device_spaceless"
+        fi;
+fi
