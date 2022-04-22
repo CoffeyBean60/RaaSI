5489d4c500e863fa9a6b89d14e165712f5d8366d
Michael Coffey <38055125+CoffeyBean60@users.noreply.github.com>
2022-04-22 11:19:25 -0500

    Add files via upload

--- Iteration2/RaaSI-main/Configuration/manuallyDeploy.sh
+++ Iteration2/RaaSI-main/Configuration/manuallyDeploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 deployment=$1
+device=$2
 
-mapfile -t node_ip < <(kubectl get nodes -o wide | awk -v x=6 '{if(NR!=1 && $3!="control-plane,plane,master") print $x}')
+mapfile -t node_ip < <(kubectl get nodes -o wide --show-labels | grep "device=$device" | awk -v x=6 '{if($3!="control-plane,plane,master") print $x}')
 
-NODE_CNT="$((${#node_ip[@]}-1))"
+NODE_CNT="${#node_ip[@]}"
 
 echo "Deploying $deployment..."
 kubectl scale deployment/"$deployment" --replicas=$NODE_CNT
