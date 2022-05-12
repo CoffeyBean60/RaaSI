b81b1e78472529110bde355d5ab369509c31f8fc
Michael Coffey <38055125+CoffeyBean60@users.noreply.github.com>
2022-05-12 13:32:25 -0500

    Update RaaSIInstallation.sh

--- Iteration2/RaaSI-main/RaaSIController/RaaSIInstallation.sh
+++ Iteration2/RaaSI-main/RaaSIController/RaaSIInstallation.sh
@@ -2,40 +2,46 @@
 
 echo "Welcome to RaaSI Component Installation"
 echo "Please select an option below:"
-echo "1. Install Load Balancers"
-echo "2. Install Primary Master Controller"
-echo "3. Install Backup Master Controllers"
-echo "4. Install GlusterFS"
-echo "5. Install Worker Nodes"
-echo "6. Return to RaaSI Welcome Page"
+echo "1. Install Standard Master Controller"
+echo "2. Install Load Balancers"
+echo "3. Install Primary Master Controller"
+echo "4. Install Backup Master Controllers"
+echo "5. Install GlusterFS"
+echo "6. Install Worker Nodes"
+echo "7. Return to RaaSI Welcome Page"
 read -r result
 
 if [[ "1" == "$result" ]]; then
+	echo "Executing Install Standard Master Controller"
+	cd ../MasterNodeSetup || exit
+	./standardMasterSetup.sh
+	cd ../RaaSIController || exit;
+elif [[ "2" == "$result" ]]; then
 	echo "Executing Install Load Balancers"
 	cd ../LoadBalancerSetup || exit
 	./LBInstallation.sh
 	cd ../RaaSIController || exit;
-elif [[ "2" == "$result" ]]; then
+elif [[ "3" == "$result" ]]; then
 	echo "Executing Install Primary Master Controller"
 	cd ../MasterNodeSetup || exit
 	./masterSetup.sh
 	cd ../RaaSIController || exit;
-elif [[ "3" == "$result" ]]; then
+elif [[ "4" == "$result" ]]; then
 	echo "Executing Install Backup Master Controllers"
 	cd ../MasterNodeSetup || exit
 	./secondaryMasterInit.sh
 	cd ../RaaSIController || exit;
-elif [[ "4" == "$result" ]]; then
+elif [[ "5" == "$result" ]]; then
 	echo "Executing Install GlusterFS"
 	cd ../GlusterSetup || exit
 	./GlusterInstallationMaster.sh
 	cd ../RaaSIController || exit;
-elif [[ "5" == "$result" ]]; then
+elif [[ "6" == "$result" ]]; then
 	echo "Executing Install Worker Nodes"
 	cd ../ClientNodeSetup || exit
 	./serverSideNodeSetup.sh
 	cd ../RaaSIController || exit;
-elif [[ "6" == "$result" ]]; then
+elif [[ "7" == "$result" ]]; then
 	clear
 	echo "Returning to RaaSI Welcome Page"
 	./RaaSIController.sh
