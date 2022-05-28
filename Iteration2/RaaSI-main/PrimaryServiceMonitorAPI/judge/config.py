e1413feda1403d6c415617ae144d6a1b4c11cbb8
Michael Coffey <38055125+CoffeyBean60@users.noreply.github.com>
2022-05-28 10:55:17 -0500

    Update config.py

--- Iteration2/RaaSI-main/PrimaryServiceMonitorAPI/judge/config.py
+++ Iteration2/RaaSI-main/PrimaryServiceMonitorAPI/judge/config.py
@@ -6,6 +6,6 @@
 DEBUG = False
 ALLOW_CONFIG = True
 POLL_TIMEOUT = 5
-SERVICES_FILE = 'services.yaml'
+SERVICES_FILE = '../services.yaml'
 # These values are overriden by defining them again in instance/config.py
 SECRET_KEY = 'supersecretkey'
