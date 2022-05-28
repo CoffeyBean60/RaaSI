e8ee0e257790f59b03b2f7f1b8da0e33222ae642
Michael Coffey <38055125+CoffeyBean60@users.noreply.github.com>
2022-05-28 11:01:36 -0500

    Update reset.sh

--- Iteration2/RaaSI-main/PrimaryServiceMonitorAPI/setup/reset.sh
+++ Iteration2/RaaSI-main/PrimaryServiceMonitorAPI/setup/reset.sh
@@ -4,8 +4,7 @@
 # Project Home: https://github.com/cobbr/Judge
 # License: GNU GPLv3
 
-celery -A judge.tasks purge -f
-cd judge || exit
+cd ../judge || exit
 export FLASK_APP=judge.py
 flask setup
 flask populate
