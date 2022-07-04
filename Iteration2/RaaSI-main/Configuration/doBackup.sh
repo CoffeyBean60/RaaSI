401c3a78f54f09b21d25abde6d25bac7473b4e52
Michael Coffey <38055125+CoffeyBean60@users.noreply.github.com>
2022-07-04 17:29:55 -0500

    Update doBackup.sh

--- Iteration2/RaaSI-main/Configuration/doBackup.sh
+++ Iteration2/RaaSI-main/Configuration/doBackup.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-response=$1
-remote_user=$2
-remote_ip=$3
+remote_user=$1
+remote_ip=$2
 
 while true
 do
 	echo "Creating backup of etcd..."
 	ETCDCTL_API=3 etcdctl snapshot save etcdBackup.db --endpoints=https://127.0.0.1:2379 --cacert=/etc/kubernetes/pki/etcd/ca.crt --cert=/etc/kubernetes/pki/etcd/server.crt --key=/etc/kubernetes/pki/etcd/server.key
 
-	if [[ "Yy" =~ $response ]]; then
-                scp etcdBackup.db "$remote_user"@"$remote_ip":backup/etcdBackup.db
-        fi;
+        scp etcdBackup.db "$remote_user"@"$remote_ip":backup/etcdBackup.db
 
         sleep 30m
 
